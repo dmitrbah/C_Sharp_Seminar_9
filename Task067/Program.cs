5ccad19824f16a89754c3477978d6f0536f3cbc0
Dmitry <dmitrbah@mail.ru>
2023-10-20 19:32:44 +0300

    Added Task 067

---
+++ Task067/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SumOfDigits(number));
+
+int SumOfDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumOfDigits(num / 10);
+}
