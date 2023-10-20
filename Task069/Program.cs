e9d70eadb45ceca50ba5455c0a2f130224a79767
Dmitry <dmitrbah@mail.ru>
2023-10-20 19:47:38 +0300

    Added Task 069

---
+++ Task069/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите число A:");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B:");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberB < 0)
+{
+    Console.WriteLine(1/(double)PowerAtoB(numberA,-numberB));
+}
+
+if (numberB > 0)
+{
+    Console.WriteLine(PowerAtoB(numberA,numberB));
+}
+
+int PowerAtoB(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA * PowerAtoB(numA, numB - 1);
+}
+
